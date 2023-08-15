7f3e30268f19ef0ca0726ab5ec5abfeec2625f00
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-08-15 14:12:14 -0600

    Add some styles to Navbar component
    Also add tailwind files and react-icon library

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
+import { IconContext } from 'react-icons'
+import { FaBars } from 'react-icons/fa6'
 import logo from '../../public/logo.svg'
+
 function Navbar() {
     return (
-        <nav>
+        <nav className='flex items-center px-6 py-10'>
             <img src={logo} alt="logo" />
-            <ul>
+            <ul className='fixed top-[100px] left-[7%] w-[86%] bg-violet text-white rounded-xl flex flex-col items-center'>
                 <li>Features</li>
                 <li>Pricing</li>
                 <li>Resources</li>
+                <hr />
+                <li>Login</li>
+                <li>Sign Up</li>
             </ul>
-            <div>
-                <div>Login</div>
-                <div>Sign Up</div>
-            </div>
+            <IconContext.Provider value={{color: 'hsl(257, 7%, 63%)', size: '1.8em', className:'ml-auto t-violete'}}>
+                <FaBars />
+            </IconContext.Provider>
         </nav>
     )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        violet: 'hsl(257, 27%, 26%)',
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
