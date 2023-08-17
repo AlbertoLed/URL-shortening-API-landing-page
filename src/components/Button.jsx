19ff2cc56994867b31a389ed9326e8d1ea498910
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-08-17 15:04:21 -0600

    Add size variant to the button and improve responsiveness in Navbar

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,13 +4,12 @@ import classNames from 'classnames'
 
 function Button({children, size, rounded, variant, className, ...props}) {
 
+    const sizeStyles = size === 'sm' ? 'py-3 px-6' : 'py-4 px-9'
     const bgColor = variant === 'applied' ? 'bg-violet-200' : 'bg-cyan'
-
     const borderRadius = rounded === 'full' ? 'rounded-full' : 'rounded-md'
 
+    const styles = classNames('hover:cursor-pointer hover:saturate-50 hover:brightness-150 text-white', sizeStyles, bgColor, borderRadius, className)
 
-
-    const styles = classNames('py-3 hover:cursor-pointer hover:saturate-50 hover:brightness-150', bgColor, borderRadius, className)
     return(
         <button className={styles} {...props} >{children}</button>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,24 +3,35 @@ import { IconContext } from 'react-icons'
 import { FaBars } from 'react-icons/fa6'
 import logo from '../../public/logo.svg'
 import Button from './Button'
+import classNames from 'classnames'
 
 function Navbar() {
     const [showMobileNav, setShowMobileNav] = useState(true)
     
     const toggleMobileNav = () => setShowMobileNav(prev => !prev)
 
+    //List sm device styles
+    const smStyles = 'fixed top-[100px] left-[7%] w-[86%] bg-violet-200 text-white rounded-xl flex flex-col items-center space-y-8 py-9 px-7 font-bold text-lg'
+
+    //List md device styles
+    const mdStyles = 'md:static md:flex md:flex-row md:w-auto md:space-y-0 md:py-0 md:px-0 md:w-full md:bg-white md:text-violet-100 md:text-sm'
+    
+    const listStyles = classNames(smStyles, mdStyles, {'fixed': showMobileNav}, {'hidden': !showMobileNav})
+
     return (
-        <nav className='flex items-center px-[7%] py-10'>
+        <nav className='flex items-center px-[7%] py-10 md:py-12 md:space-x-12 lg:px-[12%]'>
             <img src={logo} alt="logo" />
-            {showMobileNav && <ul className='fixed top-[100px] left-[7%] w-[86%] bg-violet-200 text-white rounded-xl flex flex-col items-center space-y-8 py-9 px-7 font-bold text-lg'>
+            <ul className={listStyles}>
                 <li className='hover:cursor-pointer hover:opacity-80'>Features</li>
-                <li className='hover:cursor-pointer hover:opacity-80'>Pricing</li>
-                <li className='hover:cursor-pointer hover:opacity-80'>Resources</li>
-                <hr className='w-full border-violet-100 opacity-30'/>
-                <li className='hover:cursor-pointer hover:opacity-80'>Login</li>
-                <li className='w-full'><Button variant='base' rounded='full' className='w-full'>Sign Up</Button></li>
-            </ul>}
-            <IconContext.Provider value={{color: 'hsl(257, 7%, 63%)', size: '1.8em', className:'ml-auto t-violete hover:cursor-pointer'}}>
+                <li className='hover:cursor-pointer hover:opacity-80 md:ml-7'>Pricing</li>
+                <li className='hover:cursor-pointer hover:opacity-80 md:ml-7'>Resources</li>
+                <div className='h-[1px] w-full bg-violet-100 opacity-30 md:hidden'/>
+                <li className='hover:cursor-pointer hover:opacity-80 md:ml-auto'>Login</li>
+                <li className='w-full md:w-auto md:ml-7'>
+                    <Button variant='base' rounded='full' size='sm' className='w-full md:w-max'>Sign Up</Button>
+                </li>
+            </ul>
+            <IconContext.Provider value={{color: 'hsl(257, 7%, 63%)', size: '1.8em', className:'ml-auto t-violete hover:cursor-pointer md:hidden'}}>
                 <FaBars onClick={toggleMobileNav} />
             </IconContext.Provider>
         </nav>
