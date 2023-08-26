316972de58f36fa4cbf6a2ef4a5623e6168cbda7
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-08-26 16:33:27 -0600

    Add styles to Feature component

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,10 +1,12 @@
 function Feature({title, text, image}) {
     return(
-        <div>
+        <div className="grid grid-cols-1 grid-rows-1 justify-items-center">
+            <div className="col-span-full row-span-full bg-white rounded-md px-6 pb-11 pt-[73px] mt-[45px]">
+                <h3 className="text-blue font-bold text-2xl text-center">{title}</h3>
+                <p className="text-violet-100 text-center mt-5 leading-6">{text}</p>
+            </div>
+            <div className="col-span-full row-span-full bg-violet-200 h-[90px] w-[90px] rounded-full flex items-center justify-center">
                 <img src={image} alt="icon" />
-            <div>
-                <h3>{title}</h3>
-                <p>{text}</p>
             </div>
         </div>
     )
