877305509d6c473d7f9ece82286fa6870f7bc466
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-09-11 10:41:45 -0600

    Add styles to Main and Feature for mobile perspective

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,6 +1,6 @@
-function Feature({title, text, image}) {
+function Feature({title, text, image, className}) {
     return(
-        <div className="grid grid-cols-1 grid-rows-1 justify-items-center">
+        <div className={`grid grid-cols-1 grid-rows-1 justify-items-center ${className}`}>
             <div className="col-span-full row-span-full bg-white rounded-md px-6 pb-11 pt-[73px] mt-[45px]">
                 <h3 className="text-blue font-bold text-2xl text-center">{title}</h3>
                 <p className="text-violet-100 text-center mt-5 leading-6">{text}</p>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,8 +5,6 @@ import Feature from "./Feature"
 import data from './Data.js'
 
 function Main() {
-    console.log(data)
-
     return(
         <main className="bg-gray-100 mt-[90px]">
             <div className="grid grid-cols-1 grid-rows-1">
@@ -26,19 +24,24 @@ function Main() {
                 
             </div>
             
-            <section className="mt-[90px] px-[7%]">
+            <section className="mt-[90px] px-[7%] pb-[80px]">
                 <div className="space-y-5">
                     <h2 className='text-blue text-3xl font-bold text-center'>Advanced Statistics</h2>
                     <p className='text-violet-100 text-center'>Track how your links are performing across the web with our advanced statistics dashboard.</p>
                 </div>
-                {data.map(element => 
-                <Feature 
-                    key={element.id}
-                    title={element.title}
-                    text={element.text}
-                    image={element.image}
-                />)}
-
+                <div className="relative">
+                    {data.map(element => 
+                    <Feature 
+                        key={element.id}
+                        title={element.title}
+                        text={element.text}
+                        image={element.image}
+                        className='relative z-10 pt-[50px]'
+                    />)}
+                    <div className="absolute top-[55px] left-1/2 w-[4px] h-[90%] bg-cyan z-0"></div>
+                    <div className="absolute top-[55px] right-1/2 w-[4px] h-[90%] bg-cyan z-0"></div>
+                </div>
+                
             </section>
             
             
