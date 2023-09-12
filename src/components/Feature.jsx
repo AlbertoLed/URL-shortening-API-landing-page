8c2f91565758e0043a0b0d1cd0044b35a7a00917
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-09-12 10:06:26 -0600

    Add styles in Feature component in large screens

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,11 +1,11 @@
 function Feature({title, text, image, className}) {
     return(
         <div className={`grid grid-cols-1 grid-rows-1 justify-items-center ${className}`}>
-            <div className="col-span-full row-span-full bg-white rounded-md px-6 pb-11 pt-[73px] mt-[45px]">
-                <h3 className="text-blue font-bold text-2xl text-center">{title}</h3>
-                <p className="text-violet-100 text-center mt-5 leading-6">{text}</p>
+            <div className="col-span-full row-span-full bg-white rounded-md px-6 pb-11 pt-[73px] mt-[45px] lg:px-8">
+                <h3 className="text-blue font-bold text-2xl text-center lg:text-left">{title}</h3>
+                <p className="text-violet-100 text-center mt-5 leading-6 lg:text-left">{text}</p>
             </div>
-            <div className="col-span-full row-span-full bg-violet-200 h-[90px] w-[90px] rounded-full flex items-center justify-center">
+            <div className="col-span-full row-span-full bg-violet-200 h-[90px] w-[90px] rounded-full flex items-center justify-center lg:justify-self-start lg:mx-8">
                 <img src={image} alt="icon" />
             </div>
         </div>
