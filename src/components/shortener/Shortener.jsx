adbcdbec8ba4fd7842121baa52880668919f646c
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-09-15 10:33:23 -0600

    Create state for shortenedLinks

--- src/components/shortener/Shortener.jsx
+++ src/components/shortener/Shortener.jsx
@@ -4,11 +4,26 @@ const ShortenerContext = createContext()
 
 function Shortener({children}) {
     const [currentLink, setCurrentLink] = useState('')
+    const [shortenedLinks, setShortenedLinks] = useState([])
+    console.log(shortenedLinks)
+    
+    async function shortenIt() {
+        const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${currentLink}`)
+        const data = await res.json()
+        console.log(data)
+
+        setShortenedLinks(prev => {
+            return [...prev, {
+                shortLink: data.result.full_short_link,
+                copied: false
+            }]
+        })
+    }
 
     const updateCurrentLink = (text) => setCurrentLink(text)
 
     return(
-        <ShortenerContext.Provider value={{currentLink, updateCurrentLink}}>
+        <ShortenerContext.Provider value={{currentLink, updateCurrentLink, shortenIt}}>
             <div>{children}</div>
         </ShortenerContext.Provider>
     )
--- src/components/shortener/ShortenerButton.jsx
+++ src/components/shortener/ShortenerButton.jsx
@@ -1,10 +1,14 @@
+import { useContext } from "react" 
 import Button from "../Button"
+import { ShortenerContext } from "./Shortener"
 
 function ShortenerButton({children, className}) {
+    const { shortenIt } = useContext(ShortenerContext)
     return(
         <Button 
         size='xs' 
         className={className}
+        onClick={shortenIt}
         >{children}</Button>
     )
 }
