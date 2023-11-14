8db03323dc9a54bc445c43b8b97588300900d6cd
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-11-13 19:02:08 -0600

    Change the API

--- src/components/shortener/Shortener.jsx
+++ src/components/shortener/Shortener.jsx
@@ -5,20 +5,36 @@ const ShortenerContext = createContext()
 function Shortener({children}) {
     const [currentLink, setCurrentLink] = useState('')
     const [shortenedLinks, setShortenedLinks] = useState([])
-    console.log(shortenedLinks)
-    
-    async function shortenIt() {
-        const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${currentLink}`)
-        const data = await res.json()
-        console.log(data)
 
-        setShortenedLinks(prev => {
-            return [...prev, {
-                link: currentLink,
-                shortLink: data.result.full_short_link,
-                copied: false
-            }]
-        })
+    async function shortenIt() {
+        try {
+            const res = await fetch(`https://url-shortener-service.p.rapidapi.com/shorten`, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/x-www-form-urlencoded',
+                    'X-RapidAPI-Key': '84a54c880dmsh9e134e4ace95faap1bea99jsn4bdda1ae97f5',
+                    'X-RapidAPI-Host': 'url-shortener-service.p.rapidapi.com'
+                },
+                body: new URLSearchParams({
+                    url: currentLink
+                })
+            })
+            // console.log(res)
+            const data = await res.json()
+            // console.log(data)
+    
+            setShortenedLinks(prev => {
+                return [...prev, {
+                    link: currentLink,
+                    shortLink: data.result_url,
+                    copied: false
+                }]
+            })
+        }
+        catch(error) {
+            // Show error message
+            console.log(error)
+        }
     }
 
     const updateCurrentLink = (text) => setCurrentLink(text)
